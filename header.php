88d4c401519e72359976fdc23f9673489259efdf
Clempiln <clem.piln@gmail.com>
2025-03-11 09:48:34 +0100

    création de la page a propos

--- header.php
+++ header.php
@@ -3,9 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
 <header>
-    <h1>Aquila</h1>
+    <div>
+<input id="test" type="checkbox" />
+<label for="test">
+    <svg class="burger" width="86" height="60" viewbox="0 0 150 150">
+        <g stroke-width="12">   
+            <line x1="6" y1="6" x2="80" y2="6"></line>
+            <line x1="6" y1="28" x2="80" y2="28"></line>
+            <line x1="6" y1="50" x2="80" y2="50"> </line>
+        </g>
+    </svg>
+    <svg class="close" width="86" height="60" viewbox="0 0 150 150">
+        <g stroke-width="12">   
+            <line x1="42" y1="6" x2="42" y2="80"></line>
+            <line x1="6" y1="42" x2="80" y2="42"></line>
+        </g>
+    </svg>
+</label>
+
+<div class="menu">
+<h1 class="titre">Aquila</h1>
+<hr style="width:100vw;"></hr>
+    <div>Home</div>
+    <div>About</div>
+    <div>Hello</div>
+    <div>Contact</div>
+</div>
+</div>
+<h1>Aquila</h1>
+<img class="logo" src="./logo.png" alt="logo">
 </header>
+<hr style="width:100vw;"></hr>
