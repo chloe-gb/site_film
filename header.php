a4f58b245f76fb2746b3dbe15ae7cead1e27905a
Clempiln <clem.piln@gmail.com>
2025-03-13 10:00:06 +0100

    VF

--- header.php
+++ header.php
@@ -35,15 +35,16 @@
                         <line x1="6" y1="50" x2="80" y2="50"> </line>
                     </g>
                 </svg>
-                <svg class="close" width="86" height="60" viewbox="0 0 150 150">
+            
+            </label>
+
+            <div class="menu">
+            <svg class="close" width="86" height="60" viewbox="0 0 150 150">
                     <g stroke-width="12">
                         <line x1="42" y1="6" x2="42" y2="80"></line>
                         <line x1="6" y1="42" x2="80" y2="42"></line>
                     </g>
                 </svg>
-            </label>
-
-            <div class="menu">
                 <h1 class="titre">Aquila</h1>
                 <hr style="width:100vw;">
                 </hr>
